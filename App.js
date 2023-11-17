79ca3a5d0cced22d61511171ccf3daadcd61d610
rohit-iwnl <rohitmanivel9@gmail.com>
2023-11-17 15:13:08 -0700

    walletConnect implemented

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, } from 'react-native';
+import { Button } from 'react-native-elements';
+
+import { WalletConnectModal,useWalletConnectModal } from '@walletconnect/modal-react-native'
+
+const projectId = 'a9deae8e4c1815815a60a18044a665aa'
+
+const providerMetadata = {
+  name: 'soulbound',
+  description: 'YOUR_PROJECT_DESCRIPTION',
+  url: 'https://your-project-website.com/',
+  icons: ['https://your-project-logo.com/'],
+  redirect: {
+    native: 'YOUR_APP_SCHEME://',
+    universal: 'YOUR_APP_UNIVERSAL_LINK.com'
+  }
+}
 
 export default function App() {
-  return (
+
+  const {open , isConnected ,provider,address } = useWalletConnectModal();
+
+  const onPress = () =>{
+    if(isConnected){
+      provider.disconnect()
+    }else{
+      open();
+    }
+  }
+
+  return (  
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <Text>Hello Soulbound</Text>
+    <WalletConnectModal projectId={projectId} providerMetadata={providerMetadata} />
+    <Button title="Connect Wallet" type='outline' onPress={onPress}/>
+    <Text>{isConnected? address : "Wallet not connected yet" }</Text>
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
